406462c90f422445752a2f84a3e03df0d093d1fb
vahithshetty17 <vahithshetty17@gmail.com>
2026-06-16 14:52:37 +0530

    minor changes

--- tests/Hui_test.spec.ts
+++ tests/Hui_test.spec.ts
@@ -50,6 +50,7 @@ test("Navigate to the user module", async({page})=>{
     await page.locator("//button[normalize-space()='Add New User']").click();
     await page.waitForTimeout(5000)
     await page.locator("//button[@aria-label='Close']").click();
-    console.log("vahith")
+    console.log("vahith shetty")
+    console.log("Manju")
 })
 
