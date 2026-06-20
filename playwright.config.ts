adca4da11b00730c481031d92c94db46e5210959
vahithshetty17 <vahithshetty17@gmail.com>
2026-06-20 13:20:16 +0530

    resloved config issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     // video: 'retain-on-failure',
     // screenshot: 'only-on-failure',
-    headless: false
+    headless: true
   },
 
   /* Configure projects for major browsers */
