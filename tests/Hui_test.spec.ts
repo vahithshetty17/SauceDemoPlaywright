abb98accc5d2997dc2c2483416a0e32950b7c588
vahithshetty17 <vahithshetty17@gmail.com>
2026-06-20 14:57:22 +0530

    small code changes

--- tests/Hui_test.spec.ts
+++ tests/Hui_test.spec.ts
@@ -17,6 +17,7 @@ test("add item to the cart", async({page})=>{
     await page.locator("//button[@id='add-to-cart-sauce-labs-backpack']").click();
     await page.locator("//a[@class='shopping_cart_link']").click();
     await expect(page.locator("//div[@class='inventory_item_name']")).toBeVisible();
+    await page.locator("//button[@id='checkout']").click();
 })
 
 test("Naviagte to the breadscrumb", async({page})=>{
